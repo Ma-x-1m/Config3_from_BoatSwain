555d20828bafd262d4c20c4b5088dfa6f6b459fe
Ma-x-1m <64964977+Ma-x-1m@users.noreply.github.com>
2024-12-20 13:11:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tomllib import loads
+from sys import stdin, exit
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Parser:
+    def __init__(self):
+        self.s = ''
+
+    def rec_parse(self, element, indent, key=''):
+        def add_string(string):
+            self.s += ' ' * 4 * indent
+            if key:
+                self.s += f'{key} = '
+            self.s += string
+            self.s += '\n'
+
+        if isinstance(element, int) or isinstance(element, float):
+            add_string(str(element))
+
+        elif isinstance(element, list):
+            add_string("({")
+            comas = len(element) - 1
+            for el in element:
+                self.rec_parse(el, indent + 1)
+                if comas > 0:
+                    self.s = self.s[:-1] + ',\n'
+                    comas -= 1
+            self.s += ' ' * 4 * indent
+            self.s += '})\n'
+
+        elif isinstance(element, dict):
+            add_string('dict(')
+            comas = len(element) - 1
+            for key, value in element.items():
+                self.rec_parse(value, indent + 1, key)
+                if comas > 0:
+                    self.s = self.s[:-1] + ',\n'
+                    comas -= 1
+
+            self.s += ' ' * 4 * indent
+            self.s += ')\n'
+
+    def load_toml(self, text):
+        try:
+            data = loads(text)
+        except:
+            print('РќРµРєРѕСЂСЂРµРєС‚РЅС‹Р№ РІРІРѕРґ')
+            return
+        if data == text:
+            print('РќРµРєРѕСЂСЂРµРєС‚РЅС‹Р№ РІРІРѕРґ')
+            return
+        return data
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    std_input = stdin.read()
+    parser = Parser()
+    yaml_data = parser.load_toml(std_input)
+    try:
+        parser.rec_parse(yaml_data, 0)
+    except:
+        print('Р§С‚Рѕ-С‚Рѕ РїРѕС€Р»Рѕ РЅРµ С‚Р°Рє')
+    print(parser.s)
